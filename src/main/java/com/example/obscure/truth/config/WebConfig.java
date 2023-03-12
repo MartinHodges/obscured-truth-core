ebe95f363a2e19bcb2387eaec13e09626e72fc42
Martin Hodges <martin.hodges@requillion-solutions.com>
2023-03-12 15:54:38 +1100

    Add docker and k8s support

--- src/main/java/com/example/obscure/truth/config/WebConfig.java
+++ src/main/java/com/example/obscure/truth/config/WebConfig.java
@@ -1,16 +1,22 @@
-package com.example.obscure.truth;
+package com.example.obscure.truth.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+    	log.info("Registering cors");
+        registry.addMapping("/**")
+        	.allowedOrigins("*")
+        	.allowedMethods("GET", "POST", "PUT", "OPTIONS");
     }
 }
